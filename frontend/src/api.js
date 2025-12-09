fe8a87918e4fc399a5592bc0cef20524c166c917
ghena <babiuc.ghenadii@gmail.com>
2025-12-09 17:01:33 +0200

    Dockerize frontend with Nginx and configure API proxy

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,3 +1,4 @@
-export const STORAGE_API_URL = 'http://localhost:8081/api';
-export const SYNC_API_URL = 'http://localhost:8082/api';
-export const WS_URL = 'http://localhost:8082/ws';
+export const STORAGE_API_URL = '/api/storage';
+export const SYNC_API_URL = '/api/sync';
+export const WS_URL = '/ws';
+
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    server: {
+        proxy: {
+            '/api/storage': {
+                target: 'http://localhost:8081',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api\/storage/, '/api')
+            },
+
+            '/api/sync': {
+                target: 'http://localhost:8082',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api\/sync/, '/api')
+            },
+
+            '/ws': {
+                target: 'ws://localhost:8082',
+                ws: true,
+                changeOrigin: true
+            }
+        }
+    }
 })
